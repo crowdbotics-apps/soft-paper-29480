e3568cd068d340db8ca5bffa37d5afd222e0f315
Crowdbotics <team@crowdbotics.com>
2021-08-04 22:43:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "soft_paper_29480Identifier",
+  url: "https://soft_paper_29480Identifier.botics.co",
+  api: "https://soft_paper_29480Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
